040580d72bd671c2e1e66bdea67d67337b399651
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-10-13 15:35:51 +0300

    Dialog sprite to node

--- scripts/HahmoPlaceHolder1.gd
+++ scripts/HahmoPlaceHolder1.gd
@@ -2,10 +2,9 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Dialogi.hide()
-
+	$"../DialogNode".hide()
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and event.pressed):
-		$Dialogi.show()
+		$"../DialogNode".show()
 		print("Nayta dialogi")
