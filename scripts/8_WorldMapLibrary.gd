8b20abdc79426b17863e00973a161e5066b8a9b2
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-10-18 10:31:22 +0300

    Only allows left mouse click

--- scripts/8_WorldMapLibrary.gd
+++ scripts/8_WorldMapLibrary.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 
 func _on_library_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton and event.pressed):
+	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
 		get_tree().change_scene_to_file("res://scenes/library.tscn")
 		print('Entered library')
