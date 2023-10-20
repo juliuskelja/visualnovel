045d4a7ec677f857fc09717d06185c53169fe334
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-10-20 15:25:32 +0300

    New assets

--- scripts/5_WorldMapPark.gd
+++ scripts/5_WorldMapPark.gd
@@ -2,5 +2,5 @@ extends Sprite2D
 
 func _on_park_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/park.tscn")
+		get_tree().change_scene_to_file("res://scenes/parkmap.tscn")
 		print('Entered park')
