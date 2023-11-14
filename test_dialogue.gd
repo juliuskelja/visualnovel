fafd602625f9c4c095bd979f2aeab2a1d022f0a3
JaniL <jani.lappalainen@musicagainstdrugs.info>
2023-11-14 13:02:05 +0200

    updated dialogue
    dialogue box now works by scene changing

--- test_dialogue.gd
+++ test_dialogue.gd
@@ -1,7 +1,21 @@
 extends BaseDialogueTestScene
 
-
 func _ready() -> void:
 	var balloon = load("res://custom_balloon/balloon.tscn").instantiate()
-	get_tree().current_scene.add_child(balloon)
+	add_child(balloon)
 	balloon.start(resource, title)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
+### Signals
+func _on_dialogue_ended(_resource: DialogueResource):
+	print("dialogue ended")
+	get_tree().change_scene_to_file("res://scenes/hahmonklikkaustesti1.tscn")
+
+
+#extends BaseDialogueTestScene
+
+
+#func _ready() -> void:
+#	var balloon = load("res://custom_balloon/balloon.tscn").instantiate()
+#	get_tree().current_scene.add_child.call_deferred(balloon)
+#	balloon.start(resource, title)
