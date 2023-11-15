cbc3dcd2330d2853b22c9816201f42a33dc3d422
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-11-15 13:52:57 +0200

    First cutscene ready
    Missing couple pics but otherwise done

--- scenes/cutscenes/cutscene1/cutscene1-8.gd
+++ scenes/cutscenes/cutscene1/cutscene1-8.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$FadeRect/AnimationPlayer.play("fade_in")
+		$FadeRect.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -10,10 +10,10 @@ func _process(delta):
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene2/cutscene2-1-3.tscn")
+		get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene1/cutscene1-9.tscn")
 		print("switch scene")
 
 func _on_rich_text_label_gui_input(event):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene2/cutscene2-1-3.tscn")
+		get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene1/cutscene1-9.tscn")
 		print("switch scene")
