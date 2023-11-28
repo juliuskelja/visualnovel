89e84801a9086b1b9d373ffc5106d32077205814
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-11-28 10:33:58 +0200

    Cutscene 1 ready (no music)
    still missing music but oh well

--- scenes/cutscenes/cutscene1/cutscene1-11.gd
+++ scenes/cutscenes/cutscene1/cutscene1-11.gd
@@ -4,14 +4,5 @@ extends Node2D
 func _ready():
 		$FadeRect.hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_area_2d_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene1/cutscene1-12.tscn")
-
-func _on_rich_text_label_gui_input(event):
-	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene1/cutscene1-12.tscn")
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/cutscenes/cutscene1/cutscene1-12.tscn")
