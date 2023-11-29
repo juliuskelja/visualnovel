926b673ff41da45b71419e6bfa5e42fe7ca10932
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-11-29 11:16:05 +0200

    Shaders added to map1

--- scripts/16WorldMapCityCenter.gd
+++ scripts/16WorldMapCityCenter.gd
@@ -4,3 +4,9 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
 		get_tree().change_scene_to_file("res://scenes/citycenter.tscn")
 		print('Entered city center')
+
+func _on_area_2d_mouse_entered():
+	use_parent_material = false;
+
+func _on_area_2d_mouse_exited():
+	use_parent_material = true;
--- scripts/3_WorldMapPier.gd
+++ scripts/3_WorldMapPier.gd
@@ -4,3 +4,12 @@ func _on_pier_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
 		get_tree().change_scene_to_file("res://scenes/pier.tscn")
 		print('Entered pier')
+
+
+func _on_pier_mouse_entered():
+	use_parent_material = false;
+	set_z_index(1)
+
+func _on_pier_mouse_exited():
+	use_parent_material = true;
+	set_z_index(0)
