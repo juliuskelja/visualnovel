063be1af00c7e1f236e76eca0297dbf5cf6d4e06
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-12-01 10:54:55 +0200

    newest 1.12.

--- scripts/4_WorldMapCartwheel.gd
+++ scripts/4_WorldMapCartwheel.gd
@@ -2,5 +2,4 @@ extends Sprite2D
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/secretending.tscn")
-		print('Secret ending reached')
+		get_tree().change_scene_to_file("res://scenes/cutscenes/endings/secretending.tscn")
