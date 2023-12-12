a127740a7a8f8b2825eae7c2dfb3ef493dc0475c
Julius Kelja <78741581+juliuskelja@users.noreply.github.com>
2023-12-12 14:32:46 +0200

    yuh2.0

--- scripts/5_WorldMapPark2.gd
+++ scripts/5_WorldMapPark2.gd
@@ -1,23 +1,46 @@
 extends Sprite2D
 
+@onready var warning = $"../Warning"
+
+var warningOpen = false
 var map_hover = load("res://assets/sound/valikkonavigointi.audio(VOLATOK).mp3")
-var map_click = load("res://assets/sound/karttavalitse.audio(VOLATOK).mp3")
+
+func openingWarn():
+	if (warning.z_index == -2):
+		warningOpen = false
+		print("is false")
+	if (warning.z_index == 1):
+		warningOpen = true
+		print("is open")
 
 func _ready():
-	pass
+	warning.z_index = -2
 
 func _on_park_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.pressed):
-		get_tree().change_scene_to_file("res://scenes/Implemented_scenes/park1.tscn")
-		SoundPlayer.set_stream(map_click)
-		SoundPlayer.play()
+		warning.z_index = 1
+		openingWarn()
 
 func _on_park_mouse_entered():
 	use_parent_material = false;
 	set_z_index(1)
 	SoundPlayer.set_stream(map_hover)
 	SoundPlayer.play()
-
 func _on_park_mouse_exited():
 	use_parent_material = true;
 	set_z_index(0)
+
+func _on_warning_no_button_pressed():
+	warning.z_index = -2
+	openingWarn()
+
+func _on_warning_yes_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Implemented_scenes/park1.tscn")
+
+
+func _on_warning_no_button_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_warning_yes_button_mouse_entered():
+	pass # Replace with function body.
