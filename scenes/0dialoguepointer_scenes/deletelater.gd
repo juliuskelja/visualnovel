241d522cb6b67b9710c3e6137461d62c1e855b5a
JaniL <jani.lappalainen@musicagainstdrugs.info>
2023-12-13 10:55:38 +0200

    update

--- scenes/0dialoguepointer_scenes/deletelater.gd
+++
@@ -1,8 +0,0 @@
-extends BaseDialogueTestScene
-
-var myresource = load("res://dialogues/deletelater.dialogue")
-
-func _ready() -> void:
-	var balloon = load("res://custom_balloon/balloon.tscn").instantiate()
-	add_child(balloon)
-	balloon.start(myresource, title)
